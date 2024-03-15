a3c62e43f02ebaa4a5545a46e7c29f696c35b198
HirumiN <hilminurullah3@gmail.com>
2024-03-15 16:19:17 +0700

    nyicil

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,44 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ['./public/**/*.{html,js}'],
   theme: {
-    extend: {},
+    container:{
+      center:true,
+      padding:'16px'
+    },
+    extend: {
+      keyframes: {
+        typing: {
+          "0%": {
+            width: "0%",
+            visibility: "hidden"
+          },
+          "100%": {
+            width: "100%"
+          }  
+        },
+        blink: {
+          "50%": {
+            borderColor: "transparent"
+          },
+          "100%": {
+            borderColor: "white"
+          }  
+        }
+      },
+      animation: {
+        typing: "typing 2s steps(20) infinite alternate, blink .7s infinite"
+      },
+      fontFamily:{
+        inter:['Inter'],
+      },
+      colors:{
+        red:'#8B0C0C',
+      },
+      screens:{
+        '2xl':'1320px'
+      }
+    },
   },
   plugins: [],
 }
